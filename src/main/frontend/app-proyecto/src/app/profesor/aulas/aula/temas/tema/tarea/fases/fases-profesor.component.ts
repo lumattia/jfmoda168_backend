5ea70b80846fac37cb0da1d7a557a968d4ce3604
mattiaximinlu <mlu481@g.educaand.es>
2024-05-15 19:24:52 +0200

    Fase hecha, refactor accordion.

--- src/main/frontend/app-proyecto/src/app/profesor/aulas/aula/temas/tema/tarea/fases/fases-profesor.component.ts
+++ src/main/frontend/app-proyecto/src/app/profesor/aulas/aula/temas/tema/tarea/fases/fases-profesor.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { Location } from '@angular/common';
 import {
   NgbAccordionBody,
   NgbAccordionButton, NgbAccordionCollapse,
   NgbAccordionDirective,
   NgbAccordionHeader,
-  NgbAccordionItem
+  NgbAccordionItem, NgbModal
 } from "@ng-bootstrap/ng-bootstrap";
 import {NgForOf, NgIf} from "@angular/common";
 import {ActivatedRoute} from "@angular/router";
@@ -14,6 +14,7 @@ import {TareaService} from "../../../../../../../services/tarea.service";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {Pregunta} from "../../../../../../../interfaces/pregunta";
 import {Respuesta} from "../../../../../../../interfaces/respuesta";
+import {ModalComponent} from "../../../../../../../util/modal/modal.component";
 
 @Component({
   selector: 'app-fases-profesor',
@@ -34,6 +35,7 @@ import {Respuesta} from "../../../../../../../interfaces/respuesta";
   styleUrl: './fases-profesor.component.css'
 })
 export class FasesProfesorComponent {
+  private modalService = inject(NgbModal);
   tarea:TareaFase=<TareaFase>{};
   show:number=0;
   constructor(route:ActivatedRoute,private tareaService:TareaService,private location: Location) {
@@ -59,6 +61,22 @@ export class FasesProfesorComponent {
     p.respuestas=[];
     p.fase= {id:this.tarea.fases[this.show].id,nombre:""};
     this.tarea.fases[this.show].preguntas.push(p)
+  }
+  openEliminarModal(i:number) {
+    const modalRef = this.modalService.open(ModalComponent);
+    modalRef.componentInstance.name = 'pregunta';
+    modalRef.componentInstance.option = {nombre:"Pregunta "+(i+1)};
+    modalRef.closed.subscribe(() => {
+      this.tarea.fases[this.show].preguntas.splice(i,1);
+    });
+  }
+  EliminarRespuesta(iPregunta:number,iRespuesta:number) {
+    const modalRef = this.modalService.open(ModalComponent);
+    modalRef.componentInstance.name = 'pregunta';
+    modalRef.componentInstance.option = {nombre:"Respuesta "+(iRespuesta+1)+" de la pregunta "+(iPregunta+1)};
+    modalRef.closed.subscribe(() => {
+      this.tarea.fases[this.show].preguntas[iPregunta].respuestas.splice(iRespuesta,1);
+    });
   }
   cancelar(){
     this.location.back()
